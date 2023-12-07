25daf2ab3e71ae42874ede31e707a6326fcd01e0
DevAnderson <Andersonofc23@gmail.com>
2023-12-07 16:27:51 -0300

    Correcao da page inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import { Flex, Text} from '@chakra-ui/react'
 
-export default function Home(){
+export default function Login(){
   return(
     <>
     <Head>
       <title>BarberPRO - Seu sistema completo</title>
     </Head>
     <Flex background="barber.900" height="100vh" alignItems="center" justifyContent="center">
-      <Text fontSize={30}>Página Home</Text>
+      <Text fontSize={30}>Landing Page</Text>
     </Flex>
     </>
   )
